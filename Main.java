64399fcfb3a2533c3dc4f7a8b4f5ec1391464605
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2023-12-21 21:34:13 +0200

    created function that reads from file and gets 2d array of numbers

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] numbers;
+        Read_File file = new Read_File();
+        numbers = file.getting_num_array();
+    }
+}
---
+++ Read_File.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Read_File {
+
+    public int[][] getting_num_array() {
+
+        String[] line;
+        final int ROWS = 9;
+        final int COLUMNS = 9;
+        int[][] numbers = new int[ROWS][COLUMNS];
+
+        try {
+            File file = new File("sudoku.txt");
+            Scanner myReader = new Scanner(file);
+
+            while (myReader.hasNextLine()) {
+                for (int i = 0; i < numbers.length; ++i) {
+                    line = myReader.nextLine().trim().split(" ");
+                    for (int j = 0; j < line.length; ++j) {
+                        numbers[i][j] = Integer.parseInt(line[j]);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Can't open file");
+            e.printStackTrace();
+        }
+
+        return numbers;
+    }
+}
