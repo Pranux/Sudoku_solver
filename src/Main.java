7e1db18cd366996c2f463600e12457a8f25aab15
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-01-31 23:47:26 +0200

    added needed functions, deleted unwanted comments

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,53 @@
 package src;
 
-public class Main
-{
-    private static boolean isSolved(int[][] board)
-    {
-        for(int i = 0; i  < 9; ++i)
-        {
-            for (int j = 0; j < 9; ++j)
-            {
+import java.util.Arrays;
+
+public class Main {
+
+    private static boolean isSolved(int[][] board) {
+        for(int i = 0; i  < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
                 if(board[i][j]== 0)
                     return false;
             }
         }
         return true;
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
+
         int[][] numbers;
-        int[][][] options;
+        int[][][] options = new int[0][][];
+        int[][][] tempOptions;
 
         ReadFile file = new ReadFile();
         FindOptionsForSquares choice = new FindOptionsForSquares();
         AddNumberToBoard add = new AddNumberToBoard();
+        CutOutOptions newOptions = new CutOutOptions();
 
         numbers = file.gettingNumArray();
 
-        /*while(!isSolved(numbers))
-        {
+        while(!isSolved(numbers)) {
+
+            tempOptions = options;
             options = choice.findOptions(numbers);
+            options = newOptions.cutOptions(options, numbers);
             numbers = add.addNum(numbers, options);
 
-            if(!add.changed)
-            {
-                System.out.println("Unsolvable (for now)");
-                break;
-            }
-        }*/
-
-        options = choice.findOptions(numbers);
-        for(int i = 0; i < 100; ++i)
-        {
-            options = choice.findOptions(numbers);
-            numbers = add.addNum(numbers, options);
-
-            if(!add.changed)
-            {
-                System.out.println("Unsolvable (for now)");
+            if(Arrays.deepEquals(tempOptions, options)) {
+                System.out.println("Unsolvable.");
                 break;
             }
         }
 
 
-        for(int i = 0; i  < 9; ++i)
-        {
-            for (int j = 0; j < 9; ++j)
-            {
-                System.out.println("Coordinates: [" + (i+1) + ", " + (j+1) + "]");
-                for (int k = 0; k < 3; ++k)
-                {
-                    System.out.print(options[i][j][k] + " ");
+        if(isSolved(numbers)) {
+            for(int i = 0; i  < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    System.out.print(numbers[i][j] + " ");
                 }
                 System.out.println();
-                for (int k = 3; k < 6; ++k)
-                {
-                    System.out.print(options[i][j][k] + " ");
-                }
-                System.out.println( );
-                for (int k = 6; k < 9; ++k)
-                {
-                    System.out.print(options[i][j][k] + " ");
-                }
-                System.out.println();
-                System.out.println();
-                //System.out.print(numbers[i][j] + " ");
             }
-            //System.out.println();
         }
     }
 }
