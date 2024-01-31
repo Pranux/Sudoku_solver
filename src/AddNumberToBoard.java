1323b468f7bf1b96918a679e76cfa6396f0e5f97
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-01-31 23:45:24 +0200

    changed coding style

--- src/AddNumberToBoard.java
+++ src/AddNumberToBoard.java
@@ -1,30 +1,25 @@
 package src;
 
-public class AddNumberToBoard
-{
+public class AddNumberToBoard {
+
     public boolean changed = false;
 
-    public int[][] addNum(int [][] board, int [][][] options)
-    {
+    public int[][] addNum(int [][] board, int [][][] options) {
+
         int onlyNumber = 0;
 
-        for(int i = 0; i  < 9; ++i)
-        {
-            for(int j = 0; j < 9; ++j)
-            {
-                for(int k = 0; k < 9; ++k)
-                {
+        for(int i = 0; i  < 9; ++i) {
+            for(int j = 0; j < 9; ++j) {
+
+                for(int k = 0; k < 9; ++k) {
                     if(options[i][j][k] != 0)
                         ++onlyNumber;
                 }
 
-                if(onlyNumber == 1)
-                {
+                if(onlyNumber == 1) {
                     changed = true;
-                    for(int k = 0; k < 9; ++k)
-                    {
-                        if(options[i][j][k] != 0)
-                        {
+                    for(int k = 0; k < 9; ++k) {
+                        if(options[i][j][k] != 0) {
                             board[i][j] = (k+1);
                             break;
                         }
@@ -37,5 +32,4 @@ public class AddNumberToBoard
 
         return board;
     }
-
 }
